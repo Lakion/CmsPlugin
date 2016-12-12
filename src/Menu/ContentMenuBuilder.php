895205d5c454ea593952bde3af30df135c8961bf
Kamil Kokot <kamil@kokot.me>
2016-12-12 13:47:45 +0100

    Configure admin menu to show content submenu

---
+++ src/Menu/ContentMenuBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lakion\SyliusCmsBundle\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class ContentMenuBuilder
+{
+    /**
+     * @param MenuBuilderEvent $event
+     */
+    public function configureMenu(MenuBuilderEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        $content = $menu
+            ->addChild('content')
+            ->setLabel('sylius.menu.admin.main.content.header')
+        ;
+        $content
+            ->addChild('static_contents', ['route' => 'sylius_admin_static_content_index'])
+            ->setLabel('sylius.menu.admin.main.content.static_contents')
+            ->setLabelAttribute('icon', 'file')
+        ;
+        $content
+            ->addChild('routes', ['route' => 'sylius_admin_route_index'])
+            ->setLabel('sylius.menu.admin.main.content.routes')
+            ->setLabelAttribute('icon', 'sitemap')
+        ;
+    }
+}
