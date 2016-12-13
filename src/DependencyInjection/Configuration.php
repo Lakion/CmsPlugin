6b15e6a0f4f8679cbb4623d8a1963f0cfc5e0278
Kamil Kokot <kamil@kokot.me>
2016-12-13 17:21:58 +0100

    Simplify & fix configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,6 +9,7 @@ use Lakion\SyliusCmsBundle\Form\Type\StaticContentType;
 use Lakion\SyliusCmsBundle\Repository\StaticContentRepository;
 use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -22,54 +23,57 @@ final class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('lakion_sylius_cms');
 
-        $this->addResourcesSection($rootNode);
+        $this->addResourcesSection($rootNode->children());
 
         return $treeBuilder;
     }
 
     /**
-     * @param ArrayNodeDefinition $node
+     * @param NodeBuilder $rootNodeBuilder
      */
-    private function addResourcesSection(ArrayNodeDefinition $node)
+    private function addResourcesSection(NodeBuilder $rootNodeBuilder)
     {
-        $node
-            ->children()
-                ->arrayNode('resources')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->arrayNode('route')
-                            ->addDefaultsIfNotSet()
-                            ->children()
-                                ->variableNode('options')->end()
-                                ->arrayNode('classes')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('model')->defaultValue(Route::class)->cannotBeEmpty()->cannotBeEmpty()->end()
-                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->end()
-                                        ->scalarNode('repository')->cannotBeEmpty()->end()
-                                        ->scalarNode('form')->defaultValue(RouteType::class)->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                        ->arrayNode('static_content')
-                            ->addDefaultsIfNotSet()
-                            ->children()
-                                ->variableNode('options')->end()
-                                ->arrayNode('classes')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('model')->defaultValue(StaticContent::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('repository')->defaultValue(StaticContentRepository::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('form')->defaultValue(StaticContentType::class)->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-            ->end()
+        $resourcesNodeBuilder = $rootNodeBuilder->arrayNode('resources')->addDefaultsIfNotSet()->children();
+
+        $this->addResourcesRouteSection($resourcesNodeBuilder);
+        $this->addResourcesStaticContentSection($resourcesNodeBuilder);
+    }
+
+    /**
+     * @param NodeBuilder $resourcesNodeBuilder
+     */
+    private function addResourcesRouteSection(NodeBuilder $resourcesNodeBuilder)
+    {
+        $resourcesNodeBuilder
+            ->arrayNode('route')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->arrayNode('classes')
+                        ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('model')->defaultValue(Route::class)->cannotBeEmpty()->end()
+                            ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                            ->scalarNode('repository')->cannotBeEmpty()->end()
+                            ->scalarNode('form')->defaultValue(RouteType::class)->cannotBeEmpty()->end()
+        ;
+    }
+
+    /**
+     * @param NodeBuilder $resourcesNodeBuilder
+     */
+    private function addResourcesStaticContentSection(NodeBuilder $resourcesNodeBuilder)
+    {
+        $resourcesNodeBuilder
+            ->arrayNode('static_content')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->arrayNode('classes')
+                        ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('model')->defaultValue(StaticContent::class)->cannotBeEmpty()->end()
+                            ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                            ->scalarNode('repository')->defaultValue(StaticContentRepository::class)->cannotBeEmpty()->end()
+                            ->scalarNode('form')->defaultValue(StaticContentType::class)->cannotBeEmpty()->end()
         ;
     }
 }
