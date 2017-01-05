4db55e369ce365e9e857c61de9f5fe738e72f79b
Kamil Kokot <kamil@kokot.me>
2017-01-05 16:13:53 +0100

    Make loading order of this bundle independent of GridBundle

--- src/DependencyInjection/LakionSyliusCmsExtension.php
+++ src/DependencyInjection/LakionSyliusCmsExtension.php
@@ -6,17 +6,18 @@ use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceE
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Parameter;
 
-final class LakionSyliusCmsExtension extends AbstractResourceExtension
+final class LakionSyliusCmsExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        $config = $this->processConfiguration($this->getConfiguration($config, $container), $config);
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
         $this->registerResources('lakion_sylius_cms', SyliusResourceBundle::DRIVER_DOCTRINE_PHPCR_ODM, $config['resources'], $container);
@@ -25,5 +26,15 @@ final class LakionSyliusCmsExtension extends AbstractResourceExtension
 
         $staticContentRepository = $container->getDefinition('lakion_sylius_cms.repository.static_content');
         $staticContentRepository->addArgument(new Parameter('cmf_content.persistence.phpcr.content_basepath'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $container->getExtensionConfig($this->getAlias()));
+
+        $this->registerResources('lakion_sylius_cms', SyliusResourceBundle::DRIVER_DOCTRINE_PHPCR_ODM, $config['resources'], $container);
     }
 }
