88e6c4e676b9e57bc68606ddbff6d12ab8f24d2a
Kamil Kokot <kamil@kokot.me>
2017-01-09 13:35:07 +0100

    Move tests to dedicated directory

--- tests/Application/app/autoload.php
+++ tests/Application/app/autoload.php
@@ -4,7 +4,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
-$loader = require __DIR__.'/../../../../vendor/autoload.php';
+$loader = require __DIR__.'/../../../vendor/autoload.php';
 require_once __DIR__ . '/AppKernel.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
